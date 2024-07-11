b8fa41edca564ada348a9ae2bde335eb7b82056c
Andreus265 <andre.ribs265@gmail.com>
2024-07-11 13:23:07 +0000

    resolução

--- exercicios/1-primeiros-passos/3-numerico.py
+++ exercicios/1-primeiros-passos/3-numerico.py
@@ -4,8 +4,22 @@ altura = 1.74
 
 # Descubra o tipo de dado de cada variável acima
 
+tipo_data = type(data_nascimento)
+tipo_idade = type(idade_numerica)
+tipo_altura = type(altura)
+
+print('altura é:',tipo_altura)
+print('data é:',tipo_data)
+print('idade é:',tipo_idade)
 
 # Realize uma operação entre dados do tipo string e inteiro
 
+idade_concatenada = data_nascimento + ' ' + str(idade_numerica)
+
+print("Idade concatenada:", idade_concatenada)
 
 # Realize uma operação entre dados do tipo inteiro e float
+
+soma_idade_altura = idade_numerica + altura
+
+print("Soma da idade com altura:", soma_idade_altura)
