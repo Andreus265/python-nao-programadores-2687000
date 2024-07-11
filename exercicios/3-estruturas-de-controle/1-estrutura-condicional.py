dfccb946cfccc5e5a30ec97627d4276331576f83
Andreus265 <andre.ribs265@gmail.com>
2024-07-11 16:42:40 +0000

    resolução

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,8 +1,18 @@
 # Declare 4 variáveis do tipo numérica
-
+numerica1 = 1
+numerica2 = 2
+numerica3 = 3
+numerica4 = 4
 
 # Crie uma estrutura condicional para comparar dois números
+if numerica1 >= numerica2:
+  print(f'{numerica1} é maior que {numerica2}')
 
+elif numerica1 >= numerica3:
+  print(f'{numerica1} é maior que {numerica3}')
+
+else:
+  print(f'{numerica1} não é maior do que os numeros {numerica2} e {numerica3}')
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
 
