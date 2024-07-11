4ce5fbd49b66eb005708bc2f63a5820cbb571a05
Andreus265 <andre.ribs265@gmail.com>
2024-07-11 12:58:48 +0000

    resolução

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -5,15 +5,25 @@
 
 # Declare variáveis do tipo string
 
+nome_completo = "André Souza"
+endereco = "Rua Martins, 265"
+curso = "Engenharia da Computação"
+
 # Declare variáveis de cada tipo numérico
 
+idade = 22
+peso = 87.7
+altura = 1.72
 
 # Declare uma variável do tipo booleano
 
-numero = '1'
-verdade = 'True'
-string = '1.2'
+tem_carro = True
 
-print(numero)
-print(verdade)
-print (string)
+# Imprimindo as variáveis para verificar
+print("Nome:", nome_completo)
+print("Endereço:", endereco)
+print("Curso:", curso)
+print("Idade:", idade)
+print("Peso:", peso)
+print("Altura:", altura)
+print("Tem carro?", tem_carro)
