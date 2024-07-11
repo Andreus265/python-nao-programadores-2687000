7f1e7d1438c7318862aac3d7a43ff499c6a4fe4e
Andreus265 <andre.ribs265@gmail.com>
2024-07-11 16:54:17 +0000

    resolução

--- exercicios/3-estruturas-de-controle/2-for-loop.py
+++ exercicios/3-estruturas-de-controle/2-for-loop.py
@@ -7,3 +7,18 @@
 
 
 # Use esse objeto iterável para criar um for loop que imprima na tela a frase "Estou aprendendo uma linguagem de programação."
+
+lista = ['Python', 'Java', 'JavaScript', 'C++', 'Ruby']
+
+# Parte 2: For loop para imprimir cada elemento da lista
+print("Elementos da lista:")
+for elementos in lista:
+    print(elementos)
+
+# Parte 3: Objeto iterável utilizando a função range()
+iteravel = range(5)  # Vai gerar números de 0 a 4 (5 elementos)
+
+# Parte 4: For loop com o objeto iterável para imprimir a frase
+print("\nFrases:")
+for _ in iteravel:
+    print("Estou aprendendo uma linguagem de programação.")
