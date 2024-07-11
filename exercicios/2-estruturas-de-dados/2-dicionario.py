33a95ea3ae802584c9d0c4b109ae16899ba0500d
Andreus265 <andre.ribs265@gmail.com>
2024-07-11 14:12:22 +0000

    resolução

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -7,12 +7,19 @@ pessoa = {'nome':'Crislaine',
           'animal_estimacao':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+print("hobby de Crislaine:", pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
 
+valores_dict = list(pessoa.values())
+print("Lista de valores do dicionário pessoa:", valores_dict)
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
 
+chaves_dict = list(pessoa.keys())
+print("Lista de chaves do dicionário pessoa:", chaves_dict)
 
 # Insira um novo par chave-valor no dicionário
+
+pessoa['cidade'] = 'São Paulo'
+print("Dicionário pessoa após inserção:", pessoa)
